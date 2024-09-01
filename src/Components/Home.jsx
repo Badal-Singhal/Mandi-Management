4703e7e120c6664261b4b3500c67a6b6e223dede
Badal-Singhal <146357764+Badal-Singhal@users.noreply.github.com>
2024-09-01 22:52:42 +0530

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 const Home = () => {
     const navigate=useNavigate();
     const location=useLocation();
-    console.log(location);
+    
 
     useEffect(()=>{
       if(location.state && location.state.message){
