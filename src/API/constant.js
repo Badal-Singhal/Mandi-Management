1f002be98c7b76bed8999f393f212997c1974670
Badal-Singhal <146357764+Badal-Singhal@users.noreply.github.com>
2024-09-11 19:55:24 +0530

    set up production

--- src/API/constant.js
+++ src/API/constant.js
@@ -1,12 +1,24 @@
 import axios from "axios";
 
-let domain = "http://localhost:8080/";
+// let domain = "http://localhost:5000/";
 
 const instance = axios.create({
-    baseURL: domain + "mandi",
-  });
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
+
+//for production
+
+// let domain = "http://34.131.74.233:8080/api";
+
+// const instance = axios.create({
+//     baseURL: domain,
+//   });
+// in package.json file
+// "homepage": "http://34.131.74.233/",
+
   
 
+  
   
 export default instance;
   
